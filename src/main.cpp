1f4549cdfe891127ab8d7c37646d9f2289a20a16
Enzo Hugonnier <enzohugonn@gmail.com>
2023-10-26 17:17:21 +0200

    led lightup

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,28 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
+// int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-  // Serial.begin()
+  // int result = myFunction(2, 3);
+  Serial.begin(115200);
+  pinMode(27, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
+  // Vary the LED brightness by changing the PWM duty cycle
+  for (int brightness = 0; brightness <= 255; brightness++) {
+    analogWrite(27, brightness);  // Set the LED brightness
+    delay(10);  // Wait for a short time
+  }
+  for (int brightness = 255; brightness >= 0; brightness--) {
+    analogWrite(27, brightness);  // Set the LED brightness
+    delay(10);  // Wait for a short time
+  }
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
