f9c7cbaf65a8b3e49e8df4e0ae142f9079dcae11
Enzo Hugonnier <hugonnierenzo@gmail.com>
2023-11-20 10:49:21 +0100

    Add led lighting on motion detect and log it

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,38 @@
 #include <Arduino.h>
 
-const int ledPin = 27;
+/*  
+    Arduino with PIR motion sensor
+    For complete project details, visit: http://RandomNerdTutorials.com/pirsensor
+    Modified by Rui Santos based on PIR sensor by Limor Fried
+*/
+ 
+int buzzer = 27;                // the pin that the buzzer is atteched to
+int sensor = 26;              // the pin that the sensor is atteched to
+int state = LOW;             // by default, no motion detected
+int val = 0;                 // variable to store the sensor status (value)
 
-void setup()
-{
-  // Initialize serial communication
-  Serial.begin(115200);
-
-  // Set the LED pin as an output
-  pinMode(ledPin, OUTPUT);
+void setup() {
+  pinMode(buzzer, OUTPUT);      // initalize buzzer as an output
+  pinMode(sensor, INPUT);    // initialize sensor as an input
+  Serial.begin(115200);        // initialize serial
 }
 
-void fadeLED(int startBrightness, int endBrightness, int step, int delayTime)
-{
-  for (int brightness = startBrightness; brightness != endBrightness; brightness += step)
-  {
-    analogWrite(ledPin, brightness);
-    delay(delayTime);
+void loop(){
+  val = digitalRead(sensor);   // read sensor value
+  if (val == HIGH) {           // check if the sensor is HIGH
+    digitalWrite(buzzer, HIGH);   // turn buzzer ON
+    
+    if (state == LOW) {
+      Serial.println("Motion detected!"); 
+      state = HIGH;       // update variable state to HIGH
+    }
+  } 
+  else {
+      digitalWrite(buzzer, LOW); // turn buzzer OFF
+      
+      if (state == HIGH){
+        Serial.println("Motion stopped!");
+        state = LOW;       // update variable state to LOW
+    }
   }
-}
-
-void loop()
-{
-  // Fade LED from off to full brightness
-  fadeLED(0, 256, 1, 10);
-
-  // Fade LED from full brightness to off
-  fadeLED(255, -1, -1, 10);
 }
